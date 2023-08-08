fab1fb86124e7c734eec951480502c27361ca1d0
madebygarzon <madebygarzon@gmail.com>
2023-08-08 14:18:05 -0500

    Update changes

--- client/src/modules/layout/components/footer-nav/index.tsx
+++ client/src/modules/layout/components/footer-nav/index.tsx
@@ -1,36 +1,111 @@
 "use client"
-
-import clsx from "clsx"
 import { useCollections } from "medusa-react"
-import Link from "next/link"
-import CountrySelect from "../country-select"
 
 const FooterNav = () => {
   const { collections } = useCollections()
-
   return (
-    <div className="content-container flex flex-col gap-y-8 pt-16 pb-8">
+    <div className="bg-blue-gf text-white content-container flex flex-col gap-y-8 pt-16 pb-8">
       <div className="flex flex-col gap-y-6 xsmall:flex-row items-start justify-between">
-        <div>
-          <Link href="/" className="text-xl-semi uppercase">
-            Gudfy
-          </Link>
-        </div>
-        <div className="text-small-regular grid grid-cols-2 gap-x-16">
+        <div className="text-small-regular grid grid-cols-4 gap-x-16">
           <div className="flex flex-col gap-y-2">
-            <span className="text-base-semi">Collections</span>
-            <ul
-              className={clsx("grid grid-cols-1 gap-y-2", {
-                "grid-cols-2": (collections?.length || 0) > 4,
-              })}
-            >
-              {collections?.map((c) => (
-                <li key={c.id}>
-                  <Link href={`/collections/${c.handle}`}>{c.title}</Link>
-                </li>
-              ))}
+            <span className="text-base-semi">Medusa</span>
+            <ul className="grid grid-cols-1 gap-y-2">
+              <li>
+                <a
+                  href="https://github.com/medusajs"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  GitHub
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://docs.medusajs.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Documentation
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/medusajs/nextjs-starter-medusa"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Source code
+                </a>
+              </li>
             </ul>
           </div>
+
+          <div className="flex flex-col gap-y-2">
+            <span className="text-base-semi">Medusa</span>
+            <ul className="grid grid-cols-1 gap-y-2">
+              <li>
+                <a
+                  href="https://github.com/medusajs"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  GitHub
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://docs.medusajs.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Documentation
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/medusajs/nextjs-starter-medusa"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Source code
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="flex flex-col gap-y-2">
+            <span className="text-base-semi">Medusa</span>
+            <ul className="grid grid-cols-1 gap-y-2">
+              <li>
+                <a
+                  href="https://github.com/medusajs"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  GitHub
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://docs.medusajs.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Documentation
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/medusajs/nextjs-starter-medusa"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Source code
+                </a>
+              </li>
+            </ul>
+          </div>
+
           <div className="flex flex-col gap-y-2">
             <span className="text-base-semi">Medusa</span>
             <ul className="grid grid-cols-1 gap-y-2">
@@ -69,9 +144,6 @@ const FooterNav = () => {
         <span className="text-xsmall-regular text-gray-500">
           © Copyright 2022 gudfy
         </span>
-        <div className="min-w-[316px] flex xsmall:justify-end">
-          <CountrySelect />
-        </div>
       </div>
     </div>
   )
--- client/src/modules/layout/components/medusa-cta/index.tsx
+++ client/src/modules/layout/components/medusa-cta/index.tsx
@@ -1,7 +1,7 @@
 const MedusaCTA = () => {
   return (
-    <div className="py-4 flex justify-center items-center w-full">
-      <div className="content-container flex justify-center flex-1">
+    <div className="bg-blue-gf py-4 flex justify-center items-center w-full">
+      <div className="text-white content-container flex justify-center flex-1">
         <a href="https://www.gudfy.com" target="_blank" rel="noreferrer">
           Powered by Gudfy
         </a>
