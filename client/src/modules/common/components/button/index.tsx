e730d0a912a88d30422705f84c0e5154cbc62315
RonaldoDelgado <ronalldocorrea@gmail.com>
2023-08-12 11:58:29 -0500

    login y register

--- client/src/modules/common/components/button/index.tsx
+++ client/src/modules/common/components/button/index.tsx
@@ -18,9 +18,9 @@ const Button = ({
     <button
       {...props}
       className={clsx(
-        "w-full uppercase flex items-center justify-center min-h-[50px] px-5 py-[10px] text-small-regular border transition-colors duration-200 disabled:opacity-50",
+        "px-10 py-[10px] text-small-regular transition-colors duration-200",
         {
-          "text-white bg-gray-900 border-gray-900 hover:bg-white hover:text-gray-900 disabled:hover:bg-gray-900 disabled:hover:text-white":
+          "text-white bg-[#402e72] hover:bg-blue-gf":
             variant === "primary",
           "text-gray-900 bg-transparent border-gray-920 hover:bg-gray-100":
             variant === "secondary",
