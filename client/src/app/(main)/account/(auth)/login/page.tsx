5056450a8e3ad52bf02c59a15f95c09da945ab6c
RonaldoDelgado <ronalldocorrea@gmail.com>
2023-08-14 15:27:09 -0500

    mejoras login

--- client/src/app/(main)/account/(auth)/login/page.tsx
+++ client/src/app/(main)/account/(auth)/login/page.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import LoginComponente from "@modules/account/components/login"
 import { Metadata } from "next"
+import Link from "next/link"
 import Image from "next/image"
 import ButtonLigth from "@modules/common/components/button_light"
 import {BsFillArrowLeftCircleFill} from "react-icons/bs"
@@ -15,12 +16,14 @@ export default function Login() {
     <div className="grid grid-cols-2 h-screen ">
       <div className="bg-blue-gf ">
         <div className="flex h-[25%] m-[20px] justify-start items-center pl-9 mb-16">
+          <Link href="/">
         <Image
             alt="gudfy"
             src="/header/logo_gudfy.webp"
             width={160}
             height={120}
           />
+          </Link>
         </div>
       <div className=" flex items-center justify-center">
         <h1 className="w-[583px] text-[#FFFFFF] font-black text-center text-5xl space-y-4 ">
@@ -36,7 +39,9 @@ export default function Login() {
             Inicio
           </ButtonLigth>
           </div>
-          <p className="pt-8 felx text-[#ffffff] text-center text-[10px]">Volver al inicio | Copyright © 2023 | Powered by Gudfy | Términos y condiciones </p>
+        <div>
+          <p className="pt-8 felx text-[#ffffff] text-center text-[10px]"> <Link href={"/"}>Volver al inicio</Link> | Copyright © 2023 | Powered by Gudfy | <Link href={"https://gudfy.com/terminos-y-condiciones/"}> Términos y condiciones </Link></p>
+        </div>
       </div>
       <div className="flex items-center justify-center">
         <LoginComponente />
