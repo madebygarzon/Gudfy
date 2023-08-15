8344d13ef9b50f0f0c447292b37b0837f6cc1a6b
madebygarzon <madebygarzon@gmail.com>
2023-08-14 21:32:55 -0500

    Update text

--- client/src/modules/layout/components/cart-dropdown/index.tsx
+++ client/src/modules/layout/components/cart-dropdown/index.tsx
@@ -43,7 +43,7 @@ const CartDropdown = () => {
             className="hidden small:block absolute top-[calc(100%+1px)] right-0 bg-white border-x border-b border-gray-200 w-[382px] text-gray-900"
           >
             <div className="p-4 flex items-center justify-center">
-              <h3 className="text-large-semi">Shopping Bag</h3>
+              <h3 className="text-large-semi">Carrito de compras</h3>
             </div>
             {cart && items?.length ? (
               <>
@@ -91,7 +91,7 @@ const CartDropdown = () => {
                                 onClick={() => deleteItem(item.id)}
                               >
                                 <Trash size={14} />
-                                <span>Remove</span>
+                                <span>Remover</span>
                               </button>
                             </div>
                           </div>
@@ -114,7 +114,7 @@ const CartDropdown = () => {
                     </span>
                   </div>
                   <Link href="/cart" passHref>
-                    <Button>Go to bag</Button>
+                    <Button className="rounded-30">Ir a pagar</Button>
                   </Link>
                 </div>
               </>
@@ -124,12 +124,12 @@ const CartDropdown = () => {
                   <div className="bg-gray-900 text-small-regular flex items-center justify-center w-6 h-6 rounded-full text-white">
                     <span>0</span>
                   </div>
-                  <span>Your shopping bag is empty.</span>
+                  <span>Tu carrito está vacio.</span>
                   <div>
                     <Link href="/store">
                       <>
-                        <span className="sr-only">Go to all products page</span>
-                        <Button onClick={close}>Explore products</Button>
+                        <span className="sr-only">Ir a la tienda</span>
+                        <Button className="rounded-30" onClick={close}>Explorar productos</Button>
                       </>
                     </Link>
                   </div>
