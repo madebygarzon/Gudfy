169595afa1c459ba97f73cfd149734c17bf49152
RonaldoDelgado <ronalldocorrea@gmail.com>
2023-08-23 12:03:22 -0500

    React-Email Realizadon con SendGrid

--- server/src/subscribers/recovery-password.tsx
+++ server/src/subscribers/recovery-password.tsx
@@ -11,12 +11,7 @@ interface DataOptions {
   email: string
   token: string
 }
-interface EmailOptions {
-  templateId: string
-  from: string;
-  to: string;
-  subject: string;
-}
+
 class RecoveryPasswork {
   constructor({ eventBusService }: InjectedDependencies ){
     eventBusService.subscribe(
@@ -24,21 +19,17 @@ class RecoveryPasswork {
       this.handleRecoveryPass
     )
   }
-
-
-
   handleRecoveryPass = async (data :DataOptions) => {
     try {
-      console.log("ACA LLEGA ___________________-----------------_______________--------------")
     await sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
     const { email, token } = data
     const emailHtml = render(<Email url="https://example.com" email={email} token={token} />);
 
-  const options: EmailOptions = {
-    templateId: process.env.SENDGRID_ORDER_PLACED_ID,
+  const options = {
     from: "rdelgado@gudfy.com",
     to: email,
     subject: "recovery password",
+    html: emailHtml,
   };
 
   sendgrid.send(options);
