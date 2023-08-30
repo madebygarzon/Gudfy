759262af1ae951ae4099a3c33ddfb59f0645afc5
RonaldoDelgado <ronalldocorrea@gmail.com>
2023-08-29 20:04:22 -0500

    control formulario de registro

--- client/src/modules/account/components/register/index.tsx
+++ client/src/modules/account/components/register/index.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client"
 import { medusaClient } from "@lib/config"
 import { LOGIN_VIEW, useAccount } from "@lib/context/account-context"
 import Button from "@modules/common/components/button"
@@ -18,29 +18,97 @@ interface RegisterCredentials extends FieldValues {
 }
 
 const Register = () => {
-  const { loginView, refetchCustomer } = useAccount()
-  const [_, setCurrentView] = loginView
+  //const { loginView, refetchCustomer } = useAccount()
   const [authError, setAuthError] = useState<string | undefined>(undefined)
   const router = useRouter()
-
   const handleError = (e: Error) => {
-    setAuthError("An error occured. Please try again.")
+    setAuthError("Algo salio mal")
   }
 
   const {
     register,
     handleSubmit,
     formState: { errors, isSubmitting },
+    setError,
   } = useForm<RegisterCredentials>()
 
+  function handlerErros(credenciales: RegisterCredentials) {
+    let isValid = true
+    const validaciones: Record<keyof RegisterCredentials, RegExp> = {
+      first_name: /^[a-zA-Z\s]+$/,
+      last_name: /^[a-zA-Z\s]+$/,
+      email: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      password: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
+      phone: /^[0-9]{10}$/,
+    }
+
+    for (const campo in validaciones) {
+      if (credenciales[campo]) {
+        if (!validaciones[campo].test(credenciales[campo])) {
+          isValid = false
+          switch (campo) {
+            case "first_name":
+              setError("first_name", {
+                type: "validate",
+                message: "Solo se aceptan caracteres alfabéticos ",
+              })
+              break
+            case "last_name":
+              setError("last_name", {
+                type: "validate",
+                message: "Solo se aceptan caracteres alfabéticos ",
+              })
+              break
+            case "email":
+              setError("email", {
+                type: "validate",
+                message: "correo no aceptado ",
+              })
+              break
+            case "password":
+              setError("password", {
+                type: "validate",
+                message:
+                  "debe contener una letra minúscula, una letra mayúscula, un dígito y una longitud mínima de 8 caracteres ",
+              })
+              break
+            case "phone":
+              setError("phone", {
+                type: "validate",
+                message:
+                  "debe contener una letra minúscula, una letra mayúscula, un dígito y una longitud mínima de 8 caracteres ",
+              })
+              break
+          }
+        }
+      } else {
+        isValid = false
+      }
+    }
+
+    return isValid
+  }
+
   const onSubmit = handleSubmit(async (credentials) => {
-    await medusaClient.customers
-      .create(credentials)
-      .then(() => {
-        refetchCustomer()
-        router.push("/account")
+    const isValid = await handlerErros(credentials)
+    if (!isValid) return
+    await medusaClient.auth
+      .exists(credentials.email)
+      .then(async (e) => {
+        if (e.exists) {
+          setError("email", {
+            type: "validate",
+            message: "este correo ya esta en uso",
+          })
+        } else {
+          await medusaClient.customers
+            .create(credentials)
+            .then(() => {
+              router.push("/account")
+            })
+            .catch(handleError)
+        }
       })
-      .catch(handleError)
   })
 
   return (
@@ -52,9 +120,12 @@ const Register = () => {
       )}
       <h1 className="text-large-semi  mb-6 text-3xl">Crear una cuentar</h1>
       <p className="text-center text-base-regular text-gray-700  font-[500] mb-4">
-      ¿Ya tienes una cuenta? <Link href={"./login"}>Accede</Link> 
+        ¿Ya tienes una cuenta? <Link href={"./login"}>Accede</Link>
       </p>
-      <form className="w-full font[400] shadow-xl border-2 rounded-3xl p-10 flex flex-col items-center" onSubmit={onSubmit}>
+      <form
+        className="w-full font[400] shadow-xl border-2 rounded-3xl p-10 flex flex-col items-center"
+        onSubmit={onSubmit}
+      >
         <div className="flex flex-col w-full gap-y-2">
           <Input
             label="Nombre"
@@ -93,17 +164,18 @@ const Register = () => {
         {authError && (
           <div>
             <span className="text-rose-500 w-full text-small-regular">
-              These credentials do not match our records
+              Algunas credenciales no están permitidas
             </span>
           </div>
         )}
         <span className="text-center text-gray-700 text-small-regular mt-6">
-        Acepto {" "}
-        <Link href={"https://gudfy.com/terminos-y-condiciones/"}>los términos y condiciones.</Link>
+          Acepto{" "}
+          <Link href={"https://gudfy.com/terminos-y-condiciones/"}>
+            los términos y condiciones.
+          </Link>
         </span>
         <Button className="mt-6 rounded-full">Entrar</Button>
       </form>
-      
     </div>
   )
 }
