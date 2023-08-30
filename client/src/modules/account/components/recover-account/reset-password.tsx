be4a27eb0098abed856ca04ee75199e6ce2b75d5
RonaldoDelgado <ronalldocorrea@gmail.com>
2023-08-30 17:41:32 -0500

    validaciones extra de formulario y proceso terminado de email en el front

--- client/src/modules/account/components/recover-account/reset-password.tsx
+++ client/src/modules/account/components/recover-account/reset-password.tsx
@@ -1,68 +1,70 @@
-'use client'
-import React from  "react"
+"use client"
+import React from "react"
 import Input from "@modules/common/components/input"
 import { medusaClient } from "@lib/config"
 import Button from "@modules/common/components/button"
 import Spinner from "@modules/common/icons/spinner"
 import { FieldValues, useForm } from "react-hook-form"
+import Image from "next/image"
 
-
-type ResetCustomerPasswordFormData =  {
+type ResetCustomerPasswordFormData = {
   new_password: string
   confirm_password: string
 }
 type tokenData = {
-  token:string
-  email:string
+  token: string
+  email: string
 }
 
-const ResetPassword : React.FC<tokenData> = ({token,email}) =>{
-  const [errorMessage, setErrorMessage] = React.useState<string | undefined>(undefined)
+const ResetPassword: React.FC<tokenData> = ({ token, email }) => {
+  const [isRestored, setIsRestore] = React.useState<boolean>(false)
 
   const {
     register,
     handleSubmit,
     reset,
-    formState: { errors , isSubmitting },
+    formState: { errors, isSubmitting },
     setError,
   } = useForm<ResetCustomerPasswordFormData>()
 
   const resetPassword = async (data: ResetCustomerPasswordFormData) => {
-    console.log(data)
     if (data.new_password !== data.confirm_password) {
       setError("confirm_password", {
         type: "validate",
-        message: "Passwords do not match",
+        message: "Los campos no coinciden",
       })
-      setErrorMessage("Passwords do not match")
-
       return
     }
 
-    medusaClient.customers.resetPassword({
-      email,
-      password:data.confirm_password,
-      token,
-    })
-    .then(({ customer }) => {
-      console.log(customer.id)
-    })
-
+    medusaClient.customers
+      .resetPassword({
+        email,
+        password: data.confirm_password,
+        token,
+      })
+      .then(({ customer }) => {
+        setIsRestore(true)
+        console.log(customer.id)
+      }).catch((e)=>{
+        console.log(e)
+      })
   }
 
-  return <div>
-     {isSubmitting && (
+  return !isRestored ? (
+    <div className="max-w-md w-full flex flex-col items-center">
+      {isSubmitting && (
         <div className="z-10 fixed inset-0 bg-white bg-opacity-50 flex items-center justify-center">
           <Spinner size={24} />
         </div>
       )}
-  <form
-      onSubmit={handleSubmit(resetPassword)}
-      onReset={() => reset()}
-      className="w-full"
-    >
-      
-        <div className="grid grid-cols-2 gap-4">
+      <h1 className="text-large-semi  text-3xl">Cambiar Contraseña</h1>
+      <p className=" mb-6">{`para la el usuario con el correo ${email}`}</p>
+      <form
+        onSubmit={handleSubmit(resetPassword)}
+        onReset={() => reset()}
+        className="w-full font[400] shadow-xl border-2 rounded-3xl p-12 flex flex-col items-center "
+      >
+        <div className="flex flex-col w-full gap-y-2">
           <Input
             label="Nueva Contaseña"
             type="password"
@@ -76,9 +78,12 @@ const ResetPassword : React.FC<tokenData> = ({token,email}) =>{
             errors={errors}
           />
         </div>
-        <Button>Restablecer</Button>
-    </form>
-  </div>
+        <Button className=" mt-6 rounded-[5px]">Restablecer</Button>
+      </form>
+    </div>
+  ) : (
+    <div></div>
+  )
 }
 
 export default ResetPassword
