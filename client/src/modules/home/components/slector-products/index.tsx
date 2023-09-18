bcd7b5ed9e2dcb6941720af61d92c1ac14eb8c90
Ronaldo <ronalldocorrea@gmail.com>
2023-09-18 08:16:56 -0500

    recomendaciones de producto con boton de mas

--- client/src/modules/home/components/slector-products/index.tsx
+++ client/src/modules/home/components/slector-products/index.tsx
@@ -1,30 +1,21 @@
 "use client"
 
 import { StoreGetProductsParams } from "@medusajs/medusa"
-import InfiniteProducts from "@modules/products/components/infinite-products"
-import RefinementList from "@modules/store/components/refinement-list"
+import Recommendedproduct from "@modules/products/components/products-home-gf"
 import { useState } from "react"
 import ButonSelector from "./butons_selected"
 
 const SelectedProducts = () => {
-  const [params, setParams] = useState<StoreGetProductsParams>({})
-
+  const [params, setParams] = useState<StoreGetProductsParams>({
+    collection_id: [],
+  })
   return (
     <div className="py-6 flex flex-col items-center">
       <ButonSelector refinementList={params} setRefinementList={setParams} />
       <p className="text-[#1F0054] text-[24px] font-black pb-5 w-[90%]">
         ¡Recomendadas para ti!
       </p>
-      <InfiniteProducts params={params} />
-      <div className="flex w-full justify-center py-5">
-        <button
-          type="button"
-          onClick={() => {}}
-          className="border-blue-gf text-blue-gf border-solid  border-[1px] w-[100px] h-[48px] text-[14px] font-semibold rounded-[5px] py-2 px-2"
-        >
-          Ver más
-        </button>
-      </div>
+      <Recommendedproduct params={params} />
     </div>
   )
 }
