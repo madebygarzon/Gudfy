4aea92dad05c29b112575df4df640c71dbca6333
madebygarzon <madebygarzon@gmail.com>
2023-11-07 16:50:47 -0500

    Update code

--- client/src/modules/home/components/featured-products-second/index.tsx
+++ client/src/modules/home/components/featured-products-second/index.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { useFeaturedProductsQuery } from "@lib/hooks/use-layout-data"
-import UnderlineLink from "@modules/common/components/underline-link"
 import ProductPreview from "@modules/products/components/product-preview"
 import SkeletonProductPreview from "@modules/skeletons/components/skeleton-product-preview"
 import Link from "next/dist/client/link"
