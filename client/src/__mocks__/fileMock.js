f9c9a7c0a7dc7b95b409868a8ae5dd116cfcf32f
Ronaldo <ronalldocorrea@gmail.com>
2023-11-07 19:14:42 -0500

    jest y react testing configurados

---
+++ client/src/__mocks__/fileMock.js
@@ -0,0 +1,10 @@
+// react sin nextjs, si da error cambiar por '/test-file-stub'
+module.exports = "test-file-stub"
+
+// react con nextjs
+module.exports = {
+  src: "/img.jpg",
+  height: 24,
+  width: 24,
+  blurDataURL: "data:image/png;base64,imagedata",
+}
