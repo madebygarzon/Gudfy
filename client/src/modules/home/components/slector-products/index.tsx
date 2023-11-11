7b0bfd31b43fc5d4db12975e9b6bcd19b9a3f572
Ronaldo <ronalldocorrea@gmail.com>
2023-11-11 12:00:54 -0500

    segmentos de categorias agregadas

--- client/src/modules/home/components/slector-products/index.tsx
+++ client/src/modules/home/components/slector-products/index.tsx
@@ -4,6 +4,7 @@ import { StoreGetProductsParams } from "@medusajs/medusa"
 import Recommendedproduct from "@modules/products/components/products-home-gf"
 import { useState } from "react"
 import ButonSelector from "./butons_selected"
+import Categories from "@modules/home/components/tab-categories"
 
 const SelectedProducts = () => {
   const [params, setParams] = useState<StoreGetProductsParams>({
@@ -11,7 +12,8 @@ const SelectedProducts = () => {
   })
   return (
     <div className="py-6 flex flex-col items-center">
-      <ButonSelector refinementList={params} setRefinementList={setParams} />
+      <Categories refinementList={params} setRefinementList={setParams} />
+      {/* <ButonSelector refinementList={params} setRefinementList={setParams} /> */}
       <p className="text-[#1F0054] text-[24px] font-black pb-5 w-[90%]">
         ¡Recomendadas para ti!
       </p>
