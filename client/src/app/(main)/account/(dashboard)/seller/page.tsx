8547bdea8fc4870b6483bcbd176e1ab98eb3ec9f
Ronaldo <ronalldocorrea@gmail.com>
2023-12-01 15:43:28 -0500

    avnaces solicitud del cliente para ser vendedor

--- client/src/app/(main)/account/(dashboard)/seller/page.tsx
+++ client/src/app/(main)/account/(dashboard)/seller/page.tsx
@@ -1,4 +1,4 @@
-import SupplierTemplate from "@modules/account/templates/supplier-template"
+import SellerTemplate from "@modules/account/templates/seller-template"
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
@@ -7,5 +7,5 @@ export const metadata: Metadata = {
 }
 
 export default function Profile() {
-  return <SupplierTemplate />
+  return <SellerTemplate />
 }
