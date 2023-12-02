eeb1768270e5e7a838b4c585746b620a468989f6
Ronaldo <ronalldocorrea@gmail.com>
2023-12-02 12:06:55 -0500

    metodos y migracione para seller apllication (tabla para las solicitudes de clientes vendedores)

--- client/src/modules/account/actions/action-seller.ts
+++
@@ -1,8 +0,0 @@
-import axios, { AxiosResponse } from "axios"
-
-export async function handlerseller(customer) {
-  await axios
-    .post("http://localhost:9000/store/account/seller/", customer)
-    .then()
-    .catch((e) => console.log("error", e))
-}
