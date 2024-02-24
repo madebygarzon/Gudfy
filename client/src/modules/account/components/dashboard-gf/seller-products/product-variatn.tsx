8b19232d02f22b1230d9d0031df218cb6c610e66
RonaldoDelgado <ronalldocorrea@gmail.com>
2024-02-24 06:01:30 -0500

    creacion del producto en la tienda del customer

---
+++ client/src/modules/account/components/dashboard-gf/seller-products/product-variatn.tsx
@@ -0,0 +1,109 @@
+import React, { useEffect, useState } from "react"
+import { variant, objetOptionVariant } from "./create-product"
+import { Select as SelectNext, SelectItem, Selection } from "@nextui-org/react"
+import { Input, Spinner, Textarea } from "@nextui-org/react"
+import { DotsSix, XCircleSolid, Trash } from "@medusajs/icons"
+import { IconButton } from "@medusajs/ui"
+
+type variantType = {
+  variant: variant
+  variants: Array<variant>
+  setVaraint: React.Dispatch<React.SetStateAction<variant[]>>
+  optionVariant: Array<objetOptionVariant>
+}
+
+const ProductVariat: React.FC<variantType> = ({
+  variant,
+  variants,
+  setVaraint,
+  optionVariant,
+}) => {
+  const handlerOptionVariantControl = (
+    index: number,
+    titleOption: string,
+    titleValueVariant: string
+  ) => {
+    if (Array.isArray(variant.typeOpcionVariant)) {
+      variant.typeOpcionVariant[index] = { titleOption, titleValueVariant }
+    }
+  }
+
+  return (
+    <div className="ml-5">
+      <div className=" flex my-5 items-center   gap-2">
+        <DotsSix />
+        {optionVariant.map((obj, i) => (
+          <div key={obj.titleOption} className="flex ">
+            <SelectNext
+              label={obj.titleOption || "vacio"}
+              placeholder="Select an animal"
+              className="w-[180px]"
+              onChange={(e) => {
+                handlerOptionVariantControl(
+                  i,
+                  obj.titleOption || "",
+                  e.target.value
+                )
+              }}
+            >
+              {obj.titleValueVariant?.length ? (
+                obj.titleValueVariant?.map((objV) => (
+                  <SelectItem key={objV} textValue={objV}>
+                    {objV}
+                  </SelectItem>
+                ))
+              ) : (
+                <SelectItem key={i}>
+                  <Spinner />
+                </SelectItem>
+              )}
+            </SelectNext>
+          </div>
+        ))}
+
+        <Input
+          className="max-w-[250px]"
+          size="sm"
+          type="number"
+          label="Precio"
+          //   value={`${variant.prices}`}
+          placeholder="0.00"
+          onChange={(e) => {
+            variant.prices = parseFloat(e.target.value.replace(",", "."))
+            // setValueVariant({
+            //   ...valueVariant,
+            //   prices: parseFloat(e.target.value.replace(",", ".")),
+            // })
+          }}
+          startContent={
+            <div className="pointer-events-none flex items-center">
+              <span className="text-default-400 text-small">$</span>
+            </div>
+          }
+        />
+        <Input
+          className="max-w-[250px]"
+          size="sm"
+          type="number"
+          label="Cantidad"
+          placeholder="10"
+          //   value={`${variant.inventory_quantity}`}
+          onChange={(e) => {
+            variant.inventory_quantity = parseInt(e.target.value)
+            // setValueVariant({
+            //   ...valueVariant,
+            //   inventory_quantity: parseInt(e.target.value),
+            // })
+          }}
+          startContent={
+            <div className="pointer-events-none flex items-center">
+              <span className="text-default-400 text-small">Unidades</span>
+            </div>
+          }
+        />
+      </div>
+    </div>
+  )
+}
+
+export default ProductVariat
--- server/src/api/routes/seller/post-seller-product.ts
+++ server/src/api/routes/seller/post-seller-product.ts
@@ -9,13 +9,47 @@ export async function CreateSellerProduct(
     const imagenPath = req.file.path;
 
     const productService = req.scope.resolve("productService");
+    const dataProduct = {
+      title: productData.product.title,
+      is_giftcard: false,
+      discountable: true,
+      options: productData.optionVariant.map((option) => ({
+        title: option.titleOption,
+      })),
+      variants: productData.variant.map((v) => ({
+        title: variantTitle(v.typeOpcionVariant),
+        prices: [
+          {
+            amount: v.prices * 100,
+            currency_code: "usd",
+          },
+        ],
+        options: v.typeOpcionVariant.map((vop) => ({
+          value: vop.titleOption,
+        })),
+        inventory_quantity: v.inventory_quantity,
+      })),
+      categories: productData.categories.map((c) => ({ id: c.id })),
+    };
+
     const product = await productService.createProductStoreCustomer(
-      productData,
+      dataProduct,
       imagenPath
     );
-
     res.status(200).json(product);
   } catch (error) {
     res.status(400).json({ error });
+  }
+}
+//funsion para crear el titulo de la variante segun sus opciones
+function variantTitle(arrayObjetos) {
+  if (Array.isArray(arrayObjetos) && arrayObjetos.length > 0) {
+    // Utilizamos el método map para extraer las propiedades 'title'
+    const palabras = arrayObjetos.map((objeto) => objeto.titleValueVariant);
+    // Utilizamos el método join para unir las palabras con " / "
+    return palabras.join(" / ");
+  } else {
+    // Si el array está vacío o no es un array, simplemente devolvemos una cadena vacía
+    return "";
   }
 }
