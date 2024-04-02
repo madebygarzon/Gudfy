a52347d9355a71dfeb95d5528c98c7a137892738
madebygarzon <madebygarzon@gmail.com>
2024-04-01 20:50:29 -0500

    Progress track add

--- client/src/modules/checkout/templates/index.tsx
+++ client/src/modules/checkout/templates/index.tsx
@@ -5,28 +5,35 @@ import Link from "next/link"
 import CheckoutLoader from "../components/checkout-loader"
 import CheckoutForm from "./checkout-form"
 import CheckoutSummary from "./checkout-summary"
+import Footer from "@modules/layout/templates/footer"
+import Nav from "@modules/layout/templates/nav"
+import {TaskProgressIndicatorCheckout} from  "../../progress/index"
 
 const CheckoutTemplate = () => {
   return (
     <CheckoutProvider>
+      <Nav />
+      
+      
       <div className="bg-gray-100 relative small:min-h-screen">
         <div className="h-16 bg-white">
+        
           <nav className="flex items-center h-full justify-between content-container">
             <Link
               href="/cart"
               className="text-small-semi text-gray-700 flex items-center gap-x-2 uppercase flex-1 basis-0"
             >
               <>
+              
                 <ChevronDown className="rotate-90" size={16} />
                 <span className="mt-px hidden small:block">
-                  Back to shopping cart s
+                  Volver al carrito
                 </span>
                 <span className="mt-px block small:hidden">Back</span>
               </>
             </Link>
-            <Link href="/" className="text-xl-semi">
-              ACME
-            </Link>
+            < TaskProgressIndicatorCheckout  />
+           
             <div className="flex-1 basis-0" />
           </nav>
         </div>
@@ -37,10 +44,8 @@ const CheckoutTemplate = () => {
             <CheckoutSummary />
           </div>
         </div>
-        <div className="py-4 w-full flex items-center justify-center">
-          <MedusaCTA />
-        </div>
       </div>
+      <Footer />
     </CheckoutProvider>
   )
 }
