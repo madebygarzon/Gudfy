91c9544c1ea0b44b90f63acc65c2e64790128316
RonaldoDelgado <ronalldocorrea@gmail.com>
2024-05-24 10:51:19 -0500

    Correción peticiones a back endpoints

--- server/src/admin/actions/products/get-product-list.ts
+++ server/src/admin/actions/products/get-product-list.ts
@@ -1,6 +1,7 @@
 import Medusa from "@medusajs/medusa-js";
+import { BACKEND } from "../index";
 const medusa = new Medusa({
-  baseUrl: process.env.MEDUSA_BACKEND_URL,
+  baseUrl: BACKEND,
   maxRetries: 3,
 });
 // must be previously logged in or use api token
