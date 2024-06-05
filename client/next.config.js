7cff3d96f6331472d473eabf9909a4ee0a28d6c9
madebygarzon <madebygarzon@gmail.com>
2024-06-05 18:52:36 -0500

    Update config

--- client/next.config.js
+++ client/next.config.js
@@ -1,5 +1,6 @@
-const { withStoreConfig } = require("./store-config")
-const store = require("./store.config.json")
+const { withStoreConfig } = require("./store-config");
+const store = require("./store.config.json");
+const path = require("path");
 
 module.exports = withStoreConfig({
   experimental: {
@@ -12,8 +13,36 @@ module.exports = withStoreConfig({
       "medusa-public-images.s3.eu-west-1.amazonaws.com",
       "localhost",
       "medusa-server-testing.s3.amazonaws.com",
+      "179.61.219.62",
     ],
   },
-})
+  webpack: (config, { isServer }) => {
+    // Agregar resoluciones y alias necesarios
+    config.resolve = {
+      ...config.resolve,
+      fallback: {
+        ...config.resolve.fallback,
+        // assert: require.resolve('assert'),
+        crypto: require.resolve('crypto-browserify'),
+        // http: require.resolve('stream-http'),
+        // https: require.resolve('https-browserify'),
+        // os: require.resolve('os-browserify/browser'),
+        stream: require.resolve('stream-browserify'),
+      },
+      alias: {
+        ...config.resolve.alias,
+        'process': 'process/browser',
+      },
+    };
 
-console.log("next.config.js", JSON.stringify(module.exports, null, 2))
+    // Configuración adicional para el servidor si es necesario
+    if (isServer) {
+      // Aquí puedes agregar configuración específica del servidor si es necesario
+    }
+
+    return config;
+  },
+});
+
+console.log("next.config.js", JSON.stringify(module.exports, null, 2));
+ //test de actualización
