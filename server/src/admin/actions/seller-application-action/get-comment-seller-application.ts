7551eb89487175e920475c27bcf91de7831507ac
madebygarzon <madebygarzon@gmail.com>
2024-06-12 16:10:30 -0500

    editions enviroments

--- server/src/admin/actions/seller-application-action/get-comment-seller-application.ts
+++ server/src/admin/actions/seller-application-action/get-comment-seller-application.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
+import dotenv from "dotenv";
 //import { BACKEND } from "../index";
+
+dotenv.config({ path: '.env.production' });
 const BACKEND = process.env.BACKEND_URL || "http://localhost:9000";
 
 export const getCommenSellerApplication = async (customer_id: string) => {
