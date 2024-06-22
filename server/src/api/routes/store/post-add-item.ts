928765737b32b36b821ef2df627ccb1d88d54026
RonaldoDelgado <ronalldocorrea@gmail.com>
2024-06-21 23:14:01 -0500

    unificacion de ramas completada

--- server/src/api/routes/store/post-add-item.ts
+++ server/src/api/routes/store/post-add-item.ts
@@ -4,7 +4,6 @@ export default async (req: Request, res: Response): Promise<void> => {
   const { variant, store_variant_id, quantity } = req.body;
 
   const cartMarketService = req.scope.resolve("cartMarketService");
-  console.log("DATOS EN EL PUINTO FINAL", variant, store_variant_id, quantity);
   cartMarketService
     .postAddItem(req.params.id, variant, store_variant_id, quantity)
     .then((e) => {
