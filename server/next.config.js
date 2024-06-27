a8cf316c15817ba484195af8fe4805b8940cce7a
madebygarzon <madebygarzon@gmail.com>
2024-06-27 11:37:27 -0500

    Update code

--- server/next.config.js
+++ server/next.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
-// module.exports = {
-//   webpack: (config, { isServer }) => {
-//     config.resolve.alias['@medusajs/admin-ui/ui'] = path.join(__dirname, 'src/admin/ui');
-//     return config;
-//   },
-// };
+module.exports = {
+  webpack: (config) => {
+    config.resolve.alias['@medusajs/admin-ui/ui/index.html'] = path.join(__dirname, 'src/admin/ui/index.html');
+    return config;
+  },
+};
