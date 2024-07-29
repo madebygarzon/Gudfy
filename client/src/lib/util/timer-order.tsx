da924bf69e4ea44cb21cd512537bbfbd219e3a3f
RonaldoDelgado <ronalldocorrea@gmail.com>
2024-07-29 08:07:47 -0500

    Ajustes en la orden de compra, cancelacion por tiempo y manual

---
+++ client/src/lib/util/timer-order.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react"
+
+interface TimerProps {
+  creationTime: string
+}
+
+const Timer: React.FC<TimerProps> = ({ creationTime }) => {
+  const [timeLeft, setTimeLeft] = useState<number>(600) // 10 minutos en segundos
+  const [isCancelled, setIsCancelled] = useState<boolean>(false)
+
+  useEffect(() => {
+    const calculateTimeLeft = () => {
+      const creationDate = new Date(creationTime)
+      const currentDate = new Date()
+      const timeDifference = Math.floor(
+        (currentDate.getTime() - creationDate.getTime()) / 1000
+      ) // diferencia en segundos
+
+      if (timeDifference >= 600) {
+        setIsCancelled(true)
+      } else {
+        setTimeLeft(600 - timeDifference)
+      }
+    }
+
+    calculateTimeLeft()
+
+    const interval = setInterval(() => {
+      calculateTimeLeft()
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [creationTime])
+
+  if (isCancelled) {
+    return <div>Cancelado</div>
+  }
+
+  const minutes = Math.floor(timeLeft / 60)
+  const seconds = timeLeft % 60
+
+  return (
+    <div className="text-red-500">
+      {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+    </div>
+  )
+}
+
+export default Timer
+
