9b353a8e51e02ff17ebbed11ab82783b3133a826
RonaldoDelgado <ronalldocorrea@gmail.com>
2024-11-05 10:12:57 -0500

    Correccion del webhook

--- server/src/api/routes/store/binancepay_webhook.ts
+++ server/src/api/routes/store/binancepay_webhook.ts
@@ -2,23 +2,35 @@ import { Request, Response } from "express";
 
 export default async (req: Request, res: Response): Promise<void> => {
   const { bizType, bizStatus, data } = req.body;
-  const { id } = req.body;
-  if (bizType === "PAY" && bizStatus === "PAY_SUCCESS") {
-    const payData = JSON.parse(data);
-    const merchantTradeNo = payData.merchantTradeNo;
-    const paymentInfo = payData.paymentInfo;
-    const paymentInstructions = paymentInfo.paymentInstructions;
-    const curreny = paymentInstructions[0].curreny;
-    const amount = paymentInstructions[0].amount;
+  const { id } = req.params;
 
-    const ordedPaymentService = req.scope.resolve("orderPaymentService");
+  try {
+    if (bizType === "PAY" && bizStatus === "PAY_SUCCESS") {
+      const payData = JSON.parse(data);
+      console.log(
+        "-------------EL WEBHOOK FUE EXITOSO------------",
+        bizType,
+        bizStatus,
+        payData,
+        id
+      );
+      const merchantTradeNo = payData.merchantTradeNo;
+      const paymentInfo = payData.paymentInfo;
+      const paymentInstructions = paymentInfo.paymentInstructions;
+      const curreny = paymentInstructions[0].curreny;
+      const amount = paymentInstructions[0].amount;
 
-    const succes = await ordedPaymentService.successPayOrder(id);
+      const ordedPaymentService = req.scope.resolve("orderPaymentService");
 
-    res.status(200).json({ returnCode: "SUCCESS", returnMessage: null });
+      const succes = await ordedPaymentService.successPayOrder(id);
+
+      res.status(200).json({ returnCode: "SUCCESS", returnMessage: null });
+    }
+    res.status(200).json({
+      returnCode: "Failed",
+      returnMessage: `bizType::${bizType}, bizStatus::${bizStatus}`,
+    });
+  } catch (error) {
+    console.log("ERROR EN EL SERVICIO DEL WEBHOOK", error);
   }
-  res.status(200).json({
-    returnCode: "Failed",
-    returnMessage: `bizType::${bizType}, bizStatus::${bizStatus}`,
-  });
 };
