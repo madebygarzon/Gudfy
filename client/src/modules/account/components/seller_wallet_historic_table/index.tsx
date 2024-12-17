46dabe2988e95caa9bd46b9f2f2786d926c58b42
madebygarzon <madebygarzon@gmail.com>
2024-12-17 08:11:34 -0500

    Update visual setthings

--- client/src/modules/account/components/seller_wallet_historic_table/index.tsx
+++ client/src/modules/account/components/seller_wallet_historic_table/index.tsx
@@ -6,6 +6,7 @@ import { useDisclosure } from "@nextui-org/react"
 import { getWallet } from "@modules/account/actions/get-wallet"
 import { getListPayOrders } from "@modules/account/actions/get-list-seller-pay-orders"
 import handlerformatDate from "@lib/util/formatDate"
+import Loader from "@lib/loader"
 
 export type orderData = {
   produc_title: string
@@ -17,7 +18,7 @@ export type orderData = {
   customer_last_name: string
   created_date: string
   total_price_for_product: number
-  state: "Pendiente de pago" | "Finalizado"
+  state: "Cancelado" | "Pendiente de pago" | "Completado"
 }
 
 type dataWallet = {
@@ -39,19 +40,17 @@ const WalletTable = ({ wallet, setWallet }: props) => {
     // handlerGetListSellerOrder()
   }
   const [filterStatus, setFilterStatus] = useState<
-    "Pendiente de pago" | "Finalizado" | "Cancelado" | "all"
+    "Cancelado" | "Pendiente de pago" | "Completado" | "all"
   >("all")
   // const [selectOrderData, setSelectOrderData] = useState<orderData>()
 
-  const getStatusColor = (
-    status: "Pendiente de pago" | "Finalizado" | "Cancelado"
-  ): string => {
+  const getStatusColor = (status: "Cancelado" | "Pendiente de pago" | "Completado"): string => {
     switch (status) {
       case "Cancelado":
         return "bg-red-200"
       case "Pendiente de pago":
         return "bg-yellow-200"
-      case "Finalizado":
+      case "Completado":
         return "bg-blue-200"
       default:
         return ""
@@ -77,124 +76,98 @@ const WalletTable = ({ wallet, setWallet }: props) => {
 
   return (
     <div className="w-full">
-      <div className="mb-8 flex flex-col gap-y-4 ">
-        <h1 className="text-2xl-semi">Historico de transacciones</h1>
-      </div>
       <div className="flex flex-col gap-y-8 w-full">
-        <div className="flex justify-between mb-4">
+        <div className="flex flex-wrap items-center justify-between gap-4  bg-gray-50 p-4 rounded-lg shadow-sm">
           <div>
-            <label htmlFor="status-filter" className="mr-2 font-semibold ">
+            <label
+              htmlFor="status-filter"
+              className="mr-4 font-semibold text-gray-700 text-sm lg:text-base"
+            >
               Filtrar por estado:
             </label>
             <select
               id="status-filter"
-              className="p-2 shadow-sm border-x-neutral-500 rounded"
+              className="p-2 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none text-sm lg:text-base bg-white"
               value={filterStatus}
               onChange={(e) =>
                 setFilterStatus(
-                  e.target.value as "Pendiente de pago" | "Finalizado" | "all"
+                  e.target.value as
+                    | "Cancelado"
+                    | "Pendiente de pago"
+                    | "Completado"
+                    | "all"
                 )
               }
             >
               <option value="all">Todos</option>
               <option value="Cancelado">Cancelado</option>
-              <option value="Por pagar">Por pagar</option>
-              <option value="Completado">Completado</option>
-            </select>
-            <label htmlFor="date-filter" className=" ml-4 mr-2 font-semibold ">
-              Filtrar por Fecha:
-            </label>
-            <select
-              id="date-filter"
-              className="p-2 shadow-sm border-x-neutral-500 rounded"
-              value={filterStatus}
-              onChange={(e) =>
-                setFilterStatus(
-                  e.target.value as "Pendiente de pago" | "Finalizado" | "all"
-                )
-              }
-            >
-              <option value="all">Todos</option>
-              <option value="Cancelado">Cancelado</option>
-              <option value="Por pagar">Por pagar</option>
+              <option value="Pendiente de pago">Pendiente de pago</option>
               <option value="Completado">Completado</option>
             </select>
           </div>
-          <div className="flex w-auto gap-7 mr-4 font-bold">
-            <div>
-              Saldo Pendiente:{" "}
-              <span className="text-yellow-600  text-lg">
-                {" "}
-                $ {wallet.outstanding_balance}
-              </span>
-            </div>
-            <div>
-              Saldo Pagado:{" "}
-              <span className="text-gray-400-600 text-lg">
-                $ {wallet.balance_paid}{" "}
-              </span>
-            </div>
+
+          <div>
+            Saldo Pendiente:{" "}
+            <span className="text-yellow-600  text-lg">
+              {" "}
+              $ {wallet.outstanding_balance}
+            </span>
           </div>
+          <div>
+            Saldo Pagado:{" "}
+            <span className="text-gray-400-600 text-lg">
+              $ {wallet.balance_paid}{" "}
+            </span>
+          </div>         
         </div>
         <div className="overflow-x-auto">
           <table className="min-w-full bg-white  rounded-lg shadow-md">
             <thead>
               <tr>
-                <th className="px-4 py-2  bg-gray-100 text-left">Producto</th>
-                <th className="px-4 py-2  bg-gray-100 text-left">Cantidad</th>
-                <th className="px-4 py-2  bg-gray-100 text-left">Valor</th>
-                <th className="px-4 py-2  bg-gray-100 text-left">Comisión</th>
-                <th className="px-4 py-2  bg-gray-100 text-left">
-                  Numero de pedido
-                </th>
-                <th className="px-4 py-2  bg-gray-100 text-left">Neto</th>
-                <th className="px-4 py-2  bg-gray-100 text-left">Cliente</th>
-                <th className="px-4 py-2  bg-gray-100 text-left">Fecha</th>
-                <th className="px-4 py-2  bg-gray-100 text-left">Estado</th>
+                <th className="py-2 text-left">Producto</th>
+                <th className="py-2 text-left">Cantidad</th>
+                <th className="py-2 text-left">Valor</th>
+                <th className="py-2 text-left">Comisión</th>
+                <th className="py-2 text-left">Numero de pedido</th>
+                <th className="py-2 text-left">Neto</th>
+                <th className="py-2 text-left">Cliente</th>
+                <th className="py-2 text-left">Fecha</th>
+                <th className="py-2 text-left">Estado</th>
               </tr>
             </thead>
             <tbody>
               {listOrders?.length ? (
-                listOrders?.map((order, i) => (
+                listOrders
+                .filter((order) => filterStatus === "all" || order.state === filterStatus)
+                .map((order, i) => (
                   <tr key={i} className="hover:bg-gray-50">
-                    <td className="px-4 py-2 ">{order.produc_title}</td>
-                    <td className="px-4 py-2 ">{order.quantity}</td>
-                    <td className="px-4 py-2 ">$ {order.unit_price}</td>
-                    <td className="px-4 py-2 ">
+                    <td className="px-4 py-2">{order.produc_title}</td>
+                    <td className="px-4 py-2">{order.quantity}</td>
+                    <td className="px-4 py-2">$ {order.unit_price}</td>
+                    <td className="px-4 py-2">
                       $ {(order.total_price_for_product * 0.1).toFixed(2)}
                     </td>
-                    <td className="px-4 py-2 ">
-                      {handlerOrderNumber(order.number_order)}
-                    </td>
-                    <td className="px-4 py-2 ">
+                    <td className="px-4 py-2">{handlerOrderNumber(order.number_order)}</td>
+                    <td className="px-4 py-2">
                       {order.unit_price * order.quantity -
                         order.total_price_for_product * 0.1}
                     </td>
-                    <td className="px-4 py-2 ">
+                    <td className="px-4 py-2">
                       {order.customer_name + " " + order.customer_last_name}
                     </td>
-                    <td className="px-4 py-2 ">
-                      {handlerformatDate(order.created_date)}
-                    </td>
-                    <td className=" py-2">
-                      <p
-                        className={`${getStatusColor(
-                          order.state
-                        )} px-4 py-2 rounded-lg `}
-                      >
-                        {order.state === "Pendiente de pago"
-                          ? "Pendiente"
-                          : order.state === "Finalizado"
-                          ? "Finalizado"
-                          : "Pagado"}
+                    <td className="px-4 py-2">{handlerformatDate(order.created_date)}</td>
+                    <td className="py-2">
+                      <p className={`${getStatusColor(order.state)} px-4 py-2 rounded-lg`}>
+                        {order.state}
                       </p>
                     </td>
                   </tr>
                 ))
               ) : (
-                <>Cargando...</>
+                <Loader />
               )}
             </tbody>
+
           </table>
         </div>
       </div>
