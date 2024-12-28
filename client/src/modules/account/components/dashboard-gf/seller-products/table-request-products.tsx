d8ee1e661de0530f87e888e8a9aa2a1be727934b
madebygarzon <madebygarzon@gmail.com>
2024-12-28 10:50:20 -0500

    Add setthings visuals

--- client/src/modules/account/components/dashboard-gf/seller-products/table-request-products.tsx
+++ client/src/modules/account/components/dashboard-gf/seller-products/table-request-products.tsx
@@ -23,39 +23,39 @@ const RequestProductTable: React.FC = () => {
     })
   }, [])
   return (
-    <div className="overflow-x-auto">
+    <div className="overflow-y-auto">
       <table className="min-w-full bg-white border border-gray-200">
         <thead>
           <tr>
             {/* <th className="px-4 py-2 border-b">ID</th>
             <th className="px-4 py-2 border-b">Customer ID</th> */}
-            <th className="px-4 py-2 border-b">Product Title</th>
-            <th className="px-4 py-2 border-b">Image</th>
-            <th className="px-4 py-2 border-b">Description</th>
-            <th className="px-4 py-2 border-b">Variants</th>
-            <th className="px-4 py-2 border-b">Approved</th>
-            <th className="px-4 py-2 border-b">Created At</th>
+            <th className="px-4 py-2 border-b border-gray-300 text-left">Producto</th>
+            <th className="px-4 py-2 border-b border-gray-300 text-left">Imagen</th>
+            <th className="px-4 py-2 border-b border-gray-300 text-left">Descripción</th>
+            <th className="px-4 py-2 border-b border-gray-300 text-left">Variaciones</th>
+            <th className="px-4 py-2 border-b border-gray-300 text-left">Aprobado</th>
+            <th className="px-4 py-2 border-b border-gray-300 text-left">Fecha</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className="">
           {reqPro?.map((product) => (
             <tr key={product.id} className="hover:bg-gray-100">
               {/* <td className="px-4 py-2 border-b">{product.id}</td>
               <td className="px-4 py-2 border-b">{product.customer_id}</td> */}
-              <td className="px-4 py-2 border-b">{product.product_title}</td>
-              <td className="px-4 py-2 border-b">
+              <td className="px-4 py-2 border-b border-gray-300">{product.product_title}</td>
+              <td className="px-4 py-2 border-b border-gray-300">
                 <img
                   src={product.product_image}
                   alt={product.product_title}
                   className="w-12 h-12 rounded"
                 />
               </td>
-              <td className="px-4 py-2 border-b">{product.description}</td>
-              <td className="px-4 py-2 border-b">{product.variants}</td>
-              <td className="px-4 py-2 border-b">
-                {product.approved ? "Yes" : "No"}
+              <td className="px-4 py-2 border-b border-gray-300">{product.description}</td>
+              <td className="px-4 py-2 border-b border-gray-300">{product.variants}</td>
+              <td className="px-4 py-2 border-b border-gray-300">
+                {product.approved ? "Si" : "No"}
               </td>
-              <td className="px-4 py-2 border-b">
+              <td className="px-4 py-2 border-b border-gray-300">
                 {new Date(product.created_at).toLocaleDateString()}
               </td>
             </tr>
